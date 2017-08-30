336c26358b9ffb98842a89048e60d7aa08984cda
Shi Dai <dai02a@goldspur.local>
2017-08-30 23:17:20 +1000

    updating

--- fold.c
+++ fold.c
@@ -71,7 +71,7 @@ void fold_main (search_mode *s, char *pred_name)
 				    freq_phase[i][j*s->nchan + k] = T2Predictor_GetPhase(&pred,mjd,freq);
 				    freq_period[i][j*s->nchan + k] = 1.0/T2Predictor_GetFrequency(&pred,mjd,freq);   // second
 				    ncyc += 2;
-				    //printf ("test %f %d %f\n", tc[i][j*s->nchan + k], ncyc, t0[i][j*s->nchan + k]);
+				    printf ("test %f %d %f\n", tc[i][j*s->nchan + k], ncyc, t0[i][j*s->nchan + k]);
 			    }
 			    else
 			    {
@@ -79,7 +79,7 @@ void fold_main (search_mode *s, char *pred_name)
 				    freq_phase[i][j*s->nchan + k] = freq_phase[i][(j-1)*s->nchan + k];
 				    freq_period[i][j*s->nchan + k] = freq_period[i][(j-1)*s->nchan + k];   // second
 			    }
-			    printf ("test %f %d %f %Lf\n", tc[i][j*s->nchan + k], ncyc, t0[i][j*s->nchan + k], freq_period[i][j*s->nchan + k]);
+			    //printf ("test %f %d %f %Lf\n", tc[i][j*s->nchan + k], ncyc, t0[i][j*s->nchan + k], freq_period[i][j*s->nchan + k]);
 		    }
 	    }
     }
